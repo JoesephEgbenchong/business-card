5f81f1e37581f55dc1ae38e350e86f1ac57988f1
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-01-10 19:56:49 +0100

    Install reactjs & tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+     <h1 className="text-3xl font-semibold text-purple-500 text-center mt-6">Let's start building Awesomeness</h1>
+    </>
+  );
+}
+
+export default App;
